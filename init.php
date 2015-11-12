b5fda3ddada251d2151709dc531086299f281a53
Nicolas Bret <nbret@doing.fr>
2015-11-12 21:14:35 +0100

    Gestion de plusieurs fichiers de conf bdd

--- init.php
+++ init.php
@@ -1,9 +1,10 @@
 <?php
 // echo $_SERVER['DOCUMENT_ROOT'].'/modules/base/config/bdd.yml<br/>';
-if (file_exists($_SERVER['DOCUMENT_ROOT'].'/modules/base/config/bdd.yml') === true) {
-    $szFichier = $_SERVER['DOCUMENT_ROOT'].'/modules/base/config/bdd.yml';
-} elseif (file_exists($_SERVER['DOCUMENT_ROOT'].'/ressources/base/config/bdd.yml') === true) {
-    $szFichier = $_SERVER['DOCUMENT_ROOT'].'/ressources/base/config/bdd.yml';
+
+if (file_exists($_SERVER['DOCUMENT_ROOT'].'/modules/base/config/bdd_'.$GLOBALS['aParamsAppli']['environnement'].'.yml') === true) {
+    $szFichier = $_SERVER['DOCUMENT_ROOT'].'/modules/base/config/bdd_'.$GLOBALS['aParamsAppli']['environnement'].'.yml';
+} elseif (file_exists($_SERVER['DOCUMENT_ROOT'].'/ressources/base/config/bdd_'.$GLOBALS['aParamsAppli']['environnement'].'.yml') === true) {
+    $szFichier = $_SERVER['DOCUMENT_ROOT'].'/ressources/base/config/bdd_'.$GLOBALS['aParamsAppli']['environnement'].'.yml';
 }
 $data = \Spyc::YAMLLoad($szFichier);
 
