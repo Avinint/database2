40f03babad5f9a83fe2ad57ca08192099d301cf8
Alex Moulin <amoulin@doing.fr>
2022-10-24 15:31:50 +0200

    Modif CLOB(Texte)

--- lib/champ/oracle/Texte.php
+++ lib/champ/oracle/Texte.php
@@ -6,11 +6,15 @@ class Texte extends Char
 {
     public function sGetSelect($sAliasColonne = '')
     {
-        return 'TO_CHAR(' . $this->sAlias . '.' . $this->sColonne  . ') "' .  ($sAliasColonne ?:  $this->sNom) . '"';
+        return $this->sAlias . '.' . $this->sColonne  . ' "' .  ($sAliasColonne ?:  $this->sNom) . '"';
     }
 
     public function sGenererPlaceholderChampPrepare()
     {
         return "TO_CLOB(:$this->sColonne)";
+    }
+    public function mGetValeur($mValeur)
+    {
+        return stream_get_contents($mValeur);
     }
 }
